a16e7dc97bdaf200c8f4243b7e12299f834882f6
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-07-18 21:48:26 +0530

    Added third person controller class

---
+++ Source/CoreThirdPerson/Controller/CoreThirPersonController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CoreThirPersonController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORETHIRDPERSON_API ACoreThirPersonController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
