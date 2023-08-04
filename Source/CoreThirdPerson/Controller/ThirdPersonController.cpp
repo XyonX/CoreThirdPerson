aa9cae85c46b8aeb326e39c5e31e6567f4cc269b
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-08-04 19:44:42 +0530

    Moved controller code to core controller

--- Source/CoreThirdPerson/Controller/ThirdPersonController.cpp
+++ Source/CoreThirdPerson/Controller/ThirdPersonController.cpp
@@ -1,99 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/******************************************************************************
+* Project Core - Generic UE Project
+* Copyright (c) [2023] [ Joydip chakraborty ]
+* This class is part of the ProjectCore open-source project. 
+* ******************************************************************************/
 
 
 #include "ThirdPersonController.h"
-#include "EnhancedInputComponent.h"
-#include "GameFramework/PawnMovementComponent.h"
-#include "Kismet/GameplayStatics.h"
 
 
 AThirdPersonController::AThirdPersonController()
 {
-	DefaultMovementData.TurnRate=1;
-	DefaultMovementData.LookUpRate=1;
-	MovementData=DefaultMovementData;
+	
 }
 
 void AThirdPersonController::BeginPlay()
 {
 	Super::BeginPlay();
-	ThirdPersonCharacter= Cast<AThirdPersonCharacter>(GetPawn());
-	
-	if(ThirdPersonCharacter==nullptr)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "Third Person Character found Found");
-		FGenericPlatformMisc::RequestExit(false);
-	}
-	
-
-	// Load a different level to exit the game (e.g., main menu or exit screen level)
-	//UGameplayStatics::OpenLevel(this, "MainMenuLevel"); // Replace "MainMenuLevel" with the name of your desired exit screen level
-	
-	// Request the application to exit gracefully
-	
-}
-
-/** Normal Input system functions */
-
-void AThirdPersonController::OnMouseMoveXAxis(float Value)
-{
-	AddYawInput(Value*GetWorld()->DeltaTimeSeconds*MovementData.TurnRate);
-}
-
-void AThirdPersonController::OnMouseMoveYAxis(float Value)
-{
-	AddPitchInput(Value*GetWorld()->DeltaTimeSeconds*MovementData.LookUpRate);
-}
-
-/** Enhanced Input Functions */
-
-void AThirdPersonController::OnMovement(const FInputActionValue& Value)
-{
-	UPawnMovementComponent*MovementComponent = ThirdPersonCharacter->GetMovementComponent();
-
-	FRotator CtrlRotation = GetControlRotation();
-	FRotator YawRot(0.0f, CtrlRotation.Yaw, 0.f);
-	
-	FVector ForwardDirection = FRotationMatrix(YawRot).GetUnitAxis(EAxis::X);
-	FVector RightDirection = FRotationMatrix(YawRot).GetUnitAxis(EAxis::Y);
-
-
-	MovementComponent->AddInputVector(ForwardDirection*Value[0]);
-	MovementComponent->AddInputVector(RightDirection*Value[1]);
-	
-	//MovementComponent->AddInputVector(ForwardDirection * (Value[0] != 0.0f));
-	//MovementComponent->AddInputVector(RightDirection * (Value[1] != 0.0f));
-}
-
-void AThirdPersonController::OnLookUp(const FInputActionValue& Value)
-{
-	AddYawInput(Value[0]*GetWorld()->DeltaTimeSeconds*MovementData.TurnRate);
-	AddPitchInput(Value[1]*GetWorld()->DeltaTimeSeconds*MovementData.LookUpRate);
-	
 }
 
 void AThirdPersonController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
-
-	InputComponent->BindAxis(TEXT("MouseX"), this, &AThirdPersonController::OnMouseMoveXAxis);
-	InputComponent->BindAxis(TEXT("MouseY"), this, &AThirdPersonController::OnMouseMoveYAxis);
-
-
-	UEnhancedInputComponent* EnhancedInputComp =Cast <UEnhancedInputComponent>(InputComponent);
-	if(IsValid(EnhancedInputComp))
-	{
-		if(MovementAction)
-		{
-			EnhancedInputComp->BindAction(	MovementAction,ETriggerEvent::Triggered,this,&AThirdPersonController::OnMovement);
-		}
-		if(LookUpAction)
-		{
-			EnhancedInputComp->BindAction(	LookUpAction,ETriggerEvent::Triggered,this,&AThirdPersonController::OnLookUp);
-		}
-	}
-
-
-	
-	
 }
+
--- Source/CoreThirdPerson/Controller/ThirdPersonController.h
+++ Source/CoreThirdPerson/Controller/ThirdPersonController.h
@@ -1,20 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/******************************************************************************
+* Project Core - Generic UE Project
+* Copyright (c) [2023] [ Joydip chakraborty ]
+* This class is part of the ProjectCore open-source project. 
+* ******************************************************************************/
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "InputActionValue.h"
-#include "CorePlugin/Data/AnimationData.h"
-#include "CorePlugin/Data/CharacterMovementData.h"
-#include "CoreThirdPerson/Character/ThirdPersonCharacter.h"
-#include "GameFramework/PlayerController.h"
+#include "CorePlayer/Player/CorePlayerController.h"
 #include "ThirdPersonController.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class CORETHIRDPERSON_API AThirdPersonController : public APlayerController
+class CORETHIRDPERSON_API AThirdPersonController : public ACorePlayerController
 {
 	GENERATED_BODY()
 public:
@@ -22,41 +22,7 @@ public:
 	AThirdPersonController();
 	
 	virtual void BeginPlay() override;
-	
 
-	//Normal Input system 
-
-	void OnMouseMoveXAxis (float Value);
-	void OnMouseMoveYAxis (float Value);
-
-	void OnMovement (const FInputActionValue& Value);
-	void OnLookUp (const FInputActionValue& Value);
-	
-	
 	virtual void SetupInputComponent() override;
-
-
-protected:
-
-
 	
-	/** Defaults   */
-	/** Exposed to set Default Values* */
-
-	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Defaults")
-	FCharacterMovementData DefaultMovementData;
-	
-	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Defaults")
-	class UInputAction* MovementAction ;
-	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Defaults")
-	class UInputAction* LookUpAction ;
-
-
-	/** Not Supposed to expose to Designer for internal use only */
-
-	UPROPERTY()
-	AThirdPersonCharacter* ThirdPersonCharacter;
-
-	FAnimationData AnimationData;
-	FCharacterMovementData MovementData;
 };
